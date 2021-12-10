5a4a90f126cf51f24726a911e4350b35d7deb9c0
Nikolay <terleew2013@yandex.ru>
2021-12-10 10:26:05 +0000

    feat: create migration db  News and Article

--- src/src/Entity/Article.php
+++ src/src/Entity/Article.php
@@ -17,8 +17,59 @@ class Article
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $createAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=News::class, inversedBy="articals")
+     */
+    private $news;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeImmutable
+    {
+        return $this->createAt;
+    }
+
+    public function setCreateAt(\DateTimeImmutable $createAt): self
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    public function getNews(): ?News
+    {
+        return $this->news;
+    }
+
+    public function setNews(?News $news): self
+    {
+        $this->news = $news;
+
+        return $this;
     }
 }
--- src/src/Entity/News.php
+++ src/src/Entity/News.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\NewsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -17,8 +19,99 @@ class News
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isShow;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Article::class, mappedBy="news")
+     */
+    private $articals;
+
+    public function __construct()
+    {
+        $this->articals = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getIsShow(): ?bool
+    {
+        return $this->isShow;
+    }
+
+    public function setIsShow(bool $isShow): self
+    {
+        $this->isShow = $isShow;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Article[]
+     */
+    public function getArticals(): Collection
+    {
+        return $this->articals;
+    }
+
+    public function addArtical(Article $artical): self
+    {
+        if (!$this->articals->contains($artical)) {
+            $this->articals[] = $artical;
+            $artical->setNews($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArtical(Article $artical): self
+    {
+        if ($this->articals->removeElement($artical)) {
+            // set the owning side to null (unless already changed)
+            if ($artical->getNews() === $this) {
+                $artical->setNews(null);
+            }
+        }
+
+        return $this;
     }
 }
