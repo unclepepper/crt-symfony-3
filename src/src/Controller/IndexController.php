f252aa281a164381fd51c124247d897d55a83889
Nikolay <terleew2013@yandex.ru>
2021-12-10 16:08:01 +0000

    feat: create pages site and controllers

--- src/src/Controller/IndexController.php
+++ src/src/Controller/IndexController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\News;
+
+use Doctrine\Persistence\ManagerRegistry;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $news = $doctrine->getRepository(News::class)->findAll();
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'news' => $news,
         ]);
     }
 }
